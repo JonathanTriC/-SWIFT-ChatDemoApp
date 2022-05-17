c90dcb4a3820f46e589ad2bd7e33ae1c25a17bec
Jonathan Tri <jonathantric@gmail.com>
2022-05-17 20:37:54 +0700

    Initial Commit

---
+++ ChatDemoApp/ChatDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDemoAppApp.swift
+//  ChatDemoApp
+//
+//  Created by Jonathan Tri Christianto on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
