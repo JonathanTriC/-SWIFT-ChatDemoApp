2432560f9bbc3ea169fde6a65dea831596df6243
Jonathan Tri <jonathantric@gmail.com>
2022-05-19 08:40:32 +0700

    integrate Firebase Firestore

--- ChatDemoApp/ChatDemoAppApp.swift
+++ ChatDemoApp/ChatDemoAppApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ChatDemoAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
